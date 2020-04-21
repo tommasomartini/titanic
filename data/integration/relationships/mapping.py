c049e77fdcaa886506f82feba6e0f161b722990c
tommasomartini <2669009+tommasomartini@users.noreply.github.com>
2020-04-21 21:46:56 +0200

    Add relationship mapping module

---
+++ data/integration/relationships/mapping.py
@@ -0,0 +1,108 @@
+coarse_description_to_fine_description = {
+    'parent': [
+        'mother',
+        'stepfather',
+        'father',
+    ],
+    'child': [
+        'daughter',
+        'son',
+        'stepdaughter',
+    ],
+    'sibling': [
+        'half-brother',
+        'brother?',
+        'sister',
+        'brother',
+    ],
+    'spouse': [
+        'mistress',
+        'fiancé',
+        'wife',
+        'lover',
+        'fiancée',
+        'husband',
+    ],
+    'friend': [
+        'lodger',
+        'shared cabin',
+        'travelling companion',
+        'friend',
+        'cabin companion',
+        'companion',
+        'cabin companion?',
+        'colleague',
+        'same cabin',
+    ],
+    'employee': [
+        'maid',
+        'chauffeur',
+        'employee (chauffeur)',
+        'wifes maid',
+        'landlady',
+        'nurse',
+        'valet',
+        'landlord',
+        'fathers valet',
+        'manservant',
+        'governess',
+        'nursmaid',
+        'husbands valet',
+        'nursemaid',
+        'escort',
+        'employee',
+    ],
+    'employer': [
+        'employers',
+        'employers maid',
+        'employer',
+        'employers valet',
+        'employers son',
+        'childrens nurse',
+        'mothers maid',
+        'employee (cook)',
+    ],
+    'relative': [
+        'grandmother',
+        'related',
+        'godchild',
+        'godfather',
+        'father-in-law',
+        'sister-in-law',
+        'nephew',
+        '1043', # cousin
+        'brother-in-law',
+        'in-law',
+        'aunt',
+        'son-in-law',
+        'brother-in-law?',
+        'relative',
+        'relative?',
+        'great-aunt',
+        'uncle',
+        'distant cousin',
+        'cousin',
+        'niece',
+    ],
+}
+
+
+_reciprocals = {
+    'parent': 'child',
+    'child': 'parent',
+    'employee': 'employer',
+    'employer': 'employee',
+}
+
+
+reciprocals = {
+    rel: _reciprocals.get(rel, rel)
+    for rel
+    in coarse_description_to_fine_description.keys()
+}
+
+
+fine_description_to_coarse_description = {}
+for coarse, fine_list in coarse_description_to_fine_description.items():
+    for fine in fine_list:
+        fine_description_to_coarse_description[fine] = coarse
