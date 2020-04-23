1113cb58a291293e8428d25d4b31aa7d29a1a26e
tommasomartini <2669009+tommasomartini@users.noreply.github.com>
2020-04-24 00:14:15 +0200

    Finish grouping families

---
+++ data/integration/relationships/manual_fixes.py
@@ -0,0 +1,108 @@
+manual_fixes = {
+    '/titanic-victim/johanna-persdotter-ahlin.html': [
+        ('sibling', '/titanic-victim/johan-emil-pettersson.html')
+    ],
+    '/titanic-victim/catherine-bourke.html': [
+        ('relative', '/titanic-victim/mary-bourke.html')
+    ],
+    '/titanic-survivor/elizabeth-catherine-brown.html': [
+        ('spouse', '/titanic-victim/thomas-william-solomon-brown.html')
+    ],
+    '/titanic-survivor/mary-wick.html': [
+        ('parent', '/titanic-survivor/mary-natalie-wick.html')
+    ],
+    '/titanic-survivor/maria-youssef-mary-thomas-touma.html': [
+        ('parent', '/titanic-survivor/hanna-youssef-touma.html')
+    ],
+    '/titanic-survivor/imanita-parrish-shelley.html': [
+        ('child', '/titanic-survivor/lutie-davis-parrish.html')
+    ],
+    '/titanic-survivor/emma-schabert.html': [
+        ('sibling', '/titanic-survivor/philipp-mock.html')
+    ],
+    '/titanic-victim/henrik-juul-hansen.html': [
+        ('sibling', '/titanic-victim/claus-peter-hansen.html'),
+        ('spouse', '/titanic-survivor/jennie-louise-hansen.html'),
+    ],
+    '/titanic-survivor/jennie-louise-hansen.html': [
+        ('relative', '/titanic-victim/claus-peter-hansen.html'),
+        ('spouse', '/titanic-victim/henrik-juul-hansen.html'),
+    ],
+    '/titanic-survivor/maria-mathilda-backstrom.html': [
+        ('sibling', '/titanic-victim/johan-birger-gustafsson.html'),
+        ('sibling', '/titanic-victim/anders-vilhelm-gustafsson.html'),
+    ],
+    '/titanic-survivor/amy-jacobsohn.html': [
+        ('spouse', '/titanic-victim/sidney-samuel-jacobsohn.html'),
+        ('sibling', '/titanic-survivor/rachel-julie-cohen-christy.html'),
+        ('parent', '/titanic-survivor/alice-frances-christy.html'),
+    ],
+    '/titanic-victim/hulda-kristina-eugenia-klasen.html': [
+        ('relative', '/titanic-victim/klas-albin-klasen.html'),
+        ('relative', '/titanic-victim/gertrud-emilia-klasen.html'),
+        ('relative', '/titanic-victim/hulda-amanda-adolfina-vestrom.html'),
+    ],
+    '/titanic-victim/klas-albin-klasen.html': [
+        ('relative', '/titanic-victim/hulda-kristina-eugenia-klasen.html'),
+        ('relative', '/titanic-victim/gertrud-emilia-klasen.html'),
+        ('relative', '/titanic-victim/hulda-amanda-adolfina-vestrom.html'),
+    ],
+    '/titanic-victim/denis-lennon.html': [
+        ('spouse', '/titanic-victim/mary-lennon-mullin.html')
+    ],
+    '/titanic-victim/joseph-charles-nicholls.html': [
+        ('parent', '/titanic-survivor/elizabeth-agnes-mary-davies.html'),
+        ('sibling', '/titanic-survivor/john-morgan-davies.html')
+    ],
+    '/titanic-victim/julius-vanderplancke.html': [
+        ('spouse', '/titanic-victim/emelie-maria-vanderplancke.html'),
+        ('sibling', '/titanic-victim/augusta-maria-vanderplancke.html'),
+        ('sibling', '/titanic-victim/leo-edmondus-vanderplancke.html'),
+    ],
+    '/titanic-victim/augusta-maria-vanderplancke.html': [
+        ('relative', '/titanic-victim/emelie-maria-vanderplancke.html'),
+        ('sibling', '/titanic-victim/julius-vanderplancke.html'),
+        ('sibling', '/titanic-victim/leo-edmondus-vanderplancke.html'),
+    ],
+    '/titanic-victim/leo-edmondus-vanderplancke.html': [
+        ('relative', '/titanic-victim/emelie-maria-vanderplancke.html'),
+        ('sibling', '/titanic-victim/julius-vanderplancke.html'),
+        ('sibling', '/titanic-victim/augusta-maria-vanderplancke.html'),
+    ],
+    '/titanic-survivor/elsie-edith-bowerman.html': [
+        ('parent', '/titanic-survivor/edith-martha-chibnall.html')
+    ],
+    '/titanic-survivor/mary-helene-douglas.html': [
+        ('parent', '/titanic-survivor/helene-baxter.html'),
+        ('sibling', '/titanic-victim/quigg-baxter.html')
+    ],
+    '/titanic-survivor/helene-baxter.html': [
+        ('child', '/titanic-victim/quigg-baxter.html'),
+        ('child', '/titanic-victim/quigg-baxter.html')
+    ],
+    '/titanic-survivor/henry-william-frauenthal.html': [
+        ('sibling', '/titanic-survivor/isaac-gerald-frauenthal.html')
+    ],
+    '/titanic-survivor/anna-hogeboom.html': [
+        ('sibling', '/titanic-survivor/kornelia-theodosia-andrews.html'),
+    ],
+    '/titanic-survivor/daisy-minahan.html': [
+        ('spouse', '/titanic-victim/william-edward-minahan.html'),
+        ('relative', '/titanic-survivor/lillian-minahan.html'),
+    ],
+    '/titanic-survivor/lillian-minahan.html': [
+        ('sibling', '/titanic-victim/william-edward-minahan.html'),
+        ('relative', '/titanic-survivor/daisy-minahan.html'),
+    ],
+    '/titanic-victim/william-edward-minahan.html': [
+        ('spouse', '/titanic-survivor/daisy-minahan.html'),
+        ('sibling', '/titanic-survivor/lillian-minahan.html'),
+    ],
+    '/titanic-survivor/omine-moubarek.html': [
+        ('child', '/titanic-survivor/gerios-george-moubarek.html'),
+        ('child', '/titanic-survivor/halim-gonios-william-george-moubarek.html'),
+    ],
+    '/titanic-survivor/ernst-ulrik-persson.html': [
+        ('sibling', '/titanic-victim/elna-matilda-strom.html')
+    ],
+}
