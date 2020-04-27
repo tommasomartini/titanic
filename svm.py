ba201f6ee9418ddc4c2f9ce813b21982bdaf0664
tommasomartini <2669009+tommasomartini@users.noreply.github.com>
2020-04-27 21:20:22 +0200

    SVM script (draft)

--- svm.py
+++ svm.py
@@ -1,33 +1,158 @@
-import pandas as pd
+from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import MaxAbsScaler, MinMaxScaler
 
 import dataset as ds
 import preprocessing as pp
+import pandas as pd
 import submission as sm
+
+'''
+Just a copy of all the attributes.
+
+attributes = [
+    'Age',
+    'BirthDate',
+    'BirthPlace',
+    'Cabin',
+    'Destination',
+    'Embarked',
+    'FirstName',
+    'Job',
+    'LastName',
+    'MaritalStatus',
+    'Nationality',
+    'Pclass',
+    'Residence',
+    'Sex',
+    'Ticket',
+    'Title',
+    'UrlId',
+    'TicketPrice',
+    'TicketNumber',
+    'BirthPlaceCountry',
+    'BirthPlaceCity',
+    'BirthPlaceRegion',
+    'ResidenceCountry',
+    'ResidenceCity',
+    'ResidenceRegion',
+    'DestinationCountry',
+    'DestinationCity',
+    'DestinationRegion',
+    'CabinDeck',
+    'AgeInDays',
+    'KPassengerId',
+    'SibSp',
+    'Parch',
+    'Split',
+    'NumChild',
+    'NumEmployee',
+    'NumEmployer',
+    'NumFriend',
+    'NumKnows',
+    'NumParent',
+    'NumRelative',
+    'NumSibling',
+    'NumSpouse',
+]
+'''
 
 
 def preprocess_dataset(df):
-    df = pp.replace_nan_embarked_with_unknown(df)
-    df = pp.replace_nan_age_with_value(df)
-    df = pp.replace_nan_fare_with_value(df)
+    keep_attributes = [
+        # 'Age',
+        # 'BirthDate',
+        # 'BirthPlace',
+        # 'Cabin',
+        # 'Destination',
+        'Embarked',
+        # 'FirstName',
+        'Job',
+        # 'LastName',
+        'MaritalStatus',
+        'Nationality',
+        'Pclass',
+        # 'Residence',
+        'Sex',
+        # 'Ticket',
+        'Title',
+        # 'UrlId',
+        'TicketPrice',
+        'TicketNumber',
+        'BirthPlaceCountry',
+        # 'BirthPlaceCity',
+        # 'BirthPlaceRegion',
+        'ResidenceCountry',
+        # 'ResidenceCity',
+        # 'ResidenceRegion',
+        'DestinationCountry',
+        # 'DestinationCity',
+        # 'DestinationRegion',
+        'CabinDeck',
+        'AgeInDays',
+        # 'KPassengerId',
+        # 'SibSp',
+        # 'Parch',
+        # 'Split',
+        'NumChild',
+        'NumEmployee',
+        'NumEmployer',
+        'NumFriend',
+        'NumKnows',
+        'NumParent',
+        'NumRelative',
+        'NumSibling',
+        'NumSpouse',
+    ]
+    drop_attributes = set(df.columns) - set(keep_attributes)
+    df = df.drop(columns=drop_attributes)
 
-    attributes_to_categorical = ['Sex', 'Embarked', 'Pclass']
+    # Missing tickets belong to crew member.
+    df['TicketNumber'] = df['TicketNumber'].fillna(-1)
+
+    # Use the median to replace missing ages.
+    no_age_df = df.loc[df['AgeInDays'].isna()]
+    for psgr_id, no_age_row in no_age_df.iterrows():
+        pclass, gender, embarked = no_age_row[['Pclass', 'Sex', 'Embarked']]
+        inferred_age = df.loc[(df['Pclass'] == pclass)
+                              & (df['Sex'] == gender)
+                              & (df['Embarked'] == embarked)
+                              & (~df['AgeInDays'].isna())]['AgeInDays'].median()
+        df.loc[psgr_id, 'AgeInDays'] = inferred_age
+
+    attributes_to_categorical = [
+        'Embarked',
+        'Job',
+        'MaritalStatus',
+        'Nationality',
+        'Pclass',
+        'Sex',
+        'Title',
+        # 'TicketPrice',
+        # 'TicketNumber',
+        'BirthPlaceCountry',
+        'ResidenceCountry',
+        'DestinationCountry',
+        'CabinDeck',
+        # 'AgeInDays',
+        # 'NumChild',
+        # 'NumEmployee',
+        # 'NumEmployer',
+        # 'NumFriend',
+        # 'NumKnows',
+        # 'NumParent',
+        # 'NumRelative',
+        # 'NumSibling',
+        # 'NumSpouse',
+    ]
+    df[attributes_to_categorical] = \
+        df[attributes_to_categorical].fillna('Unknown')
     for attribute_name in attributes_to_categorical:
         df = pp.convert_attribute_to_categorical(df, attribute_name)
 
-    df = pp.add_coarse_title_column(df)
-    df = pp.add_ticket_number_column(df)
-    df = pp.add_floor_column(df)
-
     df = pp.convert_categorical_columns_to_numerical(df)
-
-    non_numerical_attributes = ['Name', 'Ticket', 'Cabin']
-    df.drop(columns=non_numerical_attributes, inplace=True)
-
-    df = pp.sort_columns(df)
 
     return df
 
@@ -36,41 +161,40 @@ def main():
     print('SVM')
 
     X_dataset, y_dataset = ds.load_training_set()
-    X_dataset = preprocess_dataset(X_dataset)
-
     X_testset = ds.load_test_set()
+
+    X_dataset = preprocess_dataset(X_dataset)
     X_testset = preprocess_dataset(X_testset)
 
-    std_scaler = StandardScaler().fit(X_dataset)
-    X_dataset = pd.DataFrame(std_scaler.transform(X_dataset),
+    scaler = MinMaxScaler()
+    # scaler = MaxAbsScaler()
+    X_dataset = pd.DataFrame(scaler.fit_transform(X_dataset.values),
                              index=X_dataset.index,
                              columns=X_dataset.columns)
-    X_testset = pd.DataFrame(std_scaler.transform(X_testset),
+    X_testset = pd.DataFrame(scaler.transform(X_testset.values),
                              index=X_testset.index,
                              columns=X_testset.columns)
 
-    X_train, X_val, y_train, y_val = train_test_split(X_dataset,
-                                                      y_dataset,
-                                                      test_size=0.3,
-                                                      random_state=0)
+    # X_train, X_val, y_train, y_val = train_test_split(X_dataset,
+    #                                                   y_dataset,
+    #                                                   test_size=0.3,
+    #                                                   random_state=0)
 
     parameters = {
-        'C': [0.01, 0.1, 1.0, 10.],
-        'kernel': ('rbf', 'sigmoid'),
-        'gamma': ('scale', 'auto'),
+        'C': range(1, 11),
     }
-    estimator = SVC(random_state=0)
+    estimator = SVC()
     clf = GridSearchCV(estimator=estimator,
                        param_grid=parameters,
                        refit=True,
                        n_jobs=-1,
-                       cv=10)
-    clf.fit(X_train, y_train)
-    train_accuracy = clf.score(X_train, y_train)
-    val_accuracy = clf.score(X_val, y_val)
+                       cv=5)
+    clf.fit(X_dataset, y_dataset)
+    train_accuracy = clf.score(X_dataset, y_dataset)
+    # val_accuracy = clf.score(X_val, y_val)
 
     print('Train accuracy:      {}'.format(train_accuracy))
-    print('Validation accuracy: {}'.format(val_accuracy))
+    # print('Validation accuracy: {}'.format(val_accuracy))
     print('Best parameters:')
     print(clf.best_params_)
 
@@ -78,7 +202,8 @@ def main():
     print('Testset: {}/{} survived'.format(sum(test_predictions),
                                            len(test_predictions)))
 
-    X_testset = X_testset.assign(**{ds.LABEL_COLUMN_NAME: test_predictions})
+    X_testset[ds.LABEL_COLUMN_NAME] = test_predictions
+    X_testset[ds.LABEL_COLUMN_NAME] = X_testset[ds.LABEL_COLUMN_NAME].astype(int)
     sm.output_submission_file(X_testset, notes='svm')
 
 
